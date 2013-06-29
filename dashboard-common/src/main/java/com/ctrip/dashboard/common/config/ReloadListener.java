792bb2b80798c130d7169b1c43710eb8b431d97e
yongz <yonzhang2012@gmail.com>
2013-06-29 16:03:04 +0800

    delete original sources

--- dashboard-common/src/main/java/com/ctrip/dashboard/common/config/ReloadListener.java
+++
@@ -1,5 +0,0 @@
-package com.ctrip.dashboard.common.config;
-
-public interface ReloadListener {
-	public void onReloaded(String name);
-}
--- dashboard-common/src/main/java/com/ctrip/dashboard/common/config/ReloadableConfigure.java
+++
@@ -1,9 +0,0 @@
-package com.ctrip.dashboard.common.config;
-
-public interface ReloadableConfigure extends Configure{
-	public void setReloadInterval(int seconds);
-	public int getReloadInterval();
-	public void load() throws Exception;
-	public void setReloadListener(ReloadListener listener);
-	public ReloadListener getReloadListener();
-}

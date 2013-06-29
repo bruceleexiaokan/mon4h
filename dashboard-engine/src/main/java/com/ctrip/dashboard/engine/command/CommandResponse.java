79359060b7469d4dfb7481a66ac115e6b4f7b9f5
yongz <yonzhang2012@gmail.com>
2013-06-29 16:08:09 +0800

    delete original resources

--- dashboard-engine/src/main/java/com/ctrip/dashboard/engine/command/CommandResponse.java
+++
@@ -1,6 +0,0 @@
-package com.ctrip.dashboard.engine.command;
-
-public interface CommandResponse {
-	public boolean isSuccess();
-	public String build();
-}
--- dashboard-engine/src/main/java/com/ctrip/dashboard/engine/main/Server.java
+++
@@ -1,7 +0,0 @@
-package com.ctrip.dashboard.engine.main;
-
-public interface Server {
-	public void init()throws Exception;
-	public void start() throws Exception;
-	public void stop() throws Exception;
-}
